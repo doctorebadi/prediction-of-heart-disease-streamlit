d01255ac0813f8df3adf26fbe73638e53f75e3f3
Mohsen Ebadi <82760375+doctorebadi@users.noreply.github.com>
2023-01-26 18:09:56 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -9,4 +9,9 @@ X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
 from sklearn.model_selection import train_test_split # to split the dataset for training and testing 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
-st.write(X_train)
+from sklearn.svm import SVC
+svclassifier = SVC(kernel='linear')
+svclassifier.fit(X_train, y_train)
+y_pred = svclassifier.predict(X_test)
+
+st.write(y_pred)
