09074673d9fe33db2ef14092e204122b68732465
Mohsen Ebadi <82760375+doctorebadi@users.noreply.github.com>
2023-01-26 18:03:57 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,67 +1,14 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-
-st.write("""
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
-""")
-
-st.sidebar.header('User Input Parameters')
-
-def user_input_features():
-    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-    data = {'sepal_length': sepal_length,
-            'sepal_width': sepal_width,
-            'petal_length': petal_length,
-            'petal_width': petal_width}
-    features = pd.DataFrame(data, index=[0])
-    return features
-
-data1 = pd.read_csv('IRIS.csv')
-st.write(data1)
-
-
-#X= data.iloc[:, :-1].values
-#Y= data.iloc[:, -1].values
-
-
-
-df = user_input_features()
-
-st.subheader('User Input parameters')
-st.write(df)
-
-iris = datasets.load_iris()
-st.write(iris)
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
-
-st.balloons()
-
-import time
-my_bar = st.progress(0)
-for percent_complete in range(100):
-    time.sleep(0.1)
-    my_bar.progress(percent_complete + 1)
-
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn
+data = pd.read_csv('IRIS.csv')
+X = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+from sklearn.model_selection import train_test_split # to split the dataset for training and testing 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
+print(X_train.shape)
+print(y_train.shape)
+print(X_test.shape)
+print(y_test.shape)
