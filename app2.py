c1490ef78516fdafed5c44a05804ee79cd0bb96f
Mohsen Ebadi <82760375+doctorebadi@users.noreply.github.com>
2023-01-26 16:21:00 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from sklearn import datasets
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
@@ -21,36 +22,14 @@ def user_input_features():
     features = pd.DataFrame(data, index=[0])
     return features
 
+#data = pd.read_csv('IRIS.csv')
+
+#X= data.iloc[:, :-1].values
+#Y= data.iloc[:, -1].values
+
+
+
 df = user_input_features()
 
-st.subheader('User Input parameters')
-st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
-
-clf = RandomForestClassifier()
-clf.fit(X, Y)
-
-prediction = clf.predict(df)
-prediction_proba = clf.predict_proba(df)
-
-st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
-
-st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
-
-st.balloons()
-
-import time
-my_bar = st.progress(0)
-for percent_complete in range(100):
-    time.sleep(0.1)
-    my_bar.progress(percent_complete + 1)
 
