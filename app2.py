e5ca306e4c8771b24229da38d4aa5e1c07dfdf4b
Mohsen Ebadi <82760375+doctorebadi@users.noreply.github.com>
2023-01-26 18:06:19 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -8,7 +9,4 @@ X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
 from sklearn.model_selection import train_test_split # to split the dataset for training and testing 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=5)
-print(X_train.shape)
-print(y_train.shape)
-print(X_test.shape)
-print(y_test.shape)
+st.write(X_train)
