b5a81b24acf59a878b1ee86dab376e109db5e521
Mohsen Ebadi <82760375+doctorebadi@users.noreply.github.com>
2023-01-26 19:44:29 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -64,9 +64,10 @@ my_bar = st.progress(0)
 for percent_complete in range(100):
     time.sleep(0.1)
     my_bar.progress(percent_complete + 1)
+st.write(df)
     
-st.subheader('Prediction')
-z=svclassifier.predict(df)
-st.write(z[0])
+#st.subheader('Prediction')
+#z=svclassifier.predict(df)
+#st.write(z[0])
     
 
