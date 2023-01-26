fcb4ab5c3a7a6aaf2539645dd8a1074411daecfc
Mohsen Ebadi <82760375+doctorebadi@users.noreply.github.com>
2023-01-26 18:41:23 +0330

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,6 +11,27 @@ st.write("""
 # Simple Prediction of heart disease Prediction App
 """)
 
+
+st.sidebar.header('User Input Parameters')
+
+def user_input_features():
+    sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+    sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+    petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+    petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+    data = {'sepal_length': sepal_length,
+            'sepal_width': sepal_width,
+            'petal_length': petal_length,
+            'petal_width': petal_width}
+    features = pd.DataFrame(data, index=[0])
+    return features
+df = user_input_features()
+
+
+
+
+
+
 data = pd.read_csv('Data1.csv')
 X = data.iloc[:, :-1].values
 y = data.iloc[:, -1].values
